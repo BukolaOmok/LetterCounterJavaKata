182b1cdd1846f27b30a3f87f328ff105f104ed09
BOmokehinde <BOmokehinde@sis.tv>
2024-08-05 00:26:38 +0100

    add code that passes test

--- src/main/java/org/academy/lettercounter/LetterCounter.java
+++ src/main/java/org/academy/lettercounter/LetterCounter.java
@@ -1,25 +1,57 @@
 package org.academy.lettercounter;
 
-import java.util.List;
+import java.util.*;
+
 
 public class LetterCounter {
+    private final Map<Character, Integer> counts;
+
     public LetterCounter() {
+        counts = new HashMap<>();
     }
 
     public void countAll(String word) {
-        throw new RuntimeException("Not implemented");
+        char[] arrayOfChar = word.toCharArray();
+        for (char character : arrayOfChar) {
+            countOneLetter(character);
+        }
     }
 
     public List<String> generateReport() {
-        throw new RuntimeException("Not implemented");
+        List<String> resultStrings = new ArrayList<>();
+        Set<Character> letters = counts.keySet(); // why would i use set when i already have the function in count one letter that already allows me to say it should skip adding a letter that already exists and instead replace it and increment the value?
+
+        for (Character letter : letters) {
+            resultStrings.add(letter + ": " + counts.get(letter));
+        }
+        Collections.sort(resultStrings);
+        return resultStrings;
     }
 
     public int getCountForLetter(char letter) {
-        throw new RuntimeException("Not implemented");
+        Character lowerCaseLetter = Character.toLowerCase(letter);
+        return counts.getOrDefault(lowerCaseLetter, 0);
     }
 
     public void countOneLetter(Character inputLetter) {
-        throw new RuntimeException("Not implemented");
+        // Convert the input letter to lowercase
+        Character lowerCaseLetter = Character.toLowerCase(inputLetter);
+
+        // if the character is not a letter then exit and do not do any operation
+        if (!Character.isLetter(lowerCaseLetter)) {
+            return;
+        }
+
+        //If the count is not in the map then character should be the letter and integer should be 1
+        //Else if the map already contains the letter, replace what is there before, with the letter and increment the integer by 1
+
+        if (!counts.containsKey(lowerCaseLetter)) {
+            counts.put(lowerCaseLetter, 1);
+        } else {
+            counts.replace(lowerCaseLetter, counts.get(lowerCaseLetter) + 1);
+        }
+
     }
 
 }
+
